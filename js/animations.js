68ab7b8c12e62908d15ad1b878ea4f3da6fed5ce
AyazShaikh01 <sayaz5890@gmail.com>
2025-02-19 19:25:18 +0000

    Made a new Project section

---
+++ js/animations.js
@@ -0,0 +1,33 @@
+function marqueAnimation(){
+    window.addEventListener("wheel", (e) => {
+        console.log(e.deltaY);
+        if (e.deltaY > 0) {
+          gsap.to(".marquee", {
+            transform: `translateX(-200%)`, 
+            duration: 3,
+            ease: "none",
+            repeat: -1,
+          });
+          gsap.to('.logo', {
+            transform: `scaleX(-1)`, 
+              ease: "none",
+          });
+        } else {
+          const emoji = document.querySelector('.logo')
+      
+          gsap.to(".marquee", {
+              transform: `translateX(0%)`,
+              duration: 3,
+              ease: "none",
+              repeat: -1,
+          });
+          gsap.to('.logo', {
+            transform: `scaleX(1)`, 
+              ease: "none",
+          })
+        }
+      });
+     
+}
+
+marqueAnimation();
